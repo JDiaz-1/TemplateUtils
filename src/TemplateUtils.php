86fdd3a2b6cede8240698d882704ec32e0781a99
JDiaz <jdiaz@agrocolor.es>
2024-11-07 09:34:32 +0100

    TemplateUtils

--- src/TemplateUtils.php
+++ src/TemplateUtils.php
@@ -16,24 +16,16 @@ class TemplateUtils
      */
     public static function renderHtml(string $html, array $data): string
     {
-        // Patrón para encontrar variables que comienzan con $
-        $pattern = '/\$(\w+)/';
-
-        // Reemplazamos las variables por $data['variable']
-        $modifiedTemplate = preg_replace_callback($pattern, function ($matches) {
-            $variableName = $matches[1];
-            return "\$data['$variableName'] ?? null"; // Añadimos seguridad para variables inexistentes
-        }, $html);
-
         // Iniciamos el buffer de salida
         ob_start();
 
         // Extraemos las variables del array para que estén disponibles en el scope local
         extract($data, EXTR_SKIP);
 
-        // Evaluamos el contenido modificado de la plantilla
+        // Evaluamos el contenido de la plantilla
         try {
-            eval('?>' . $modifiedTemplate);
+            // Procesamos el contenido de la plantilla directamente con las variables extraídas
+            eval('?>' . $html);
         } catch (\Throwable $e) {
             // Limpiamos el buffer y lanzamos la excepción si hay un error
             ob_end_clean();
